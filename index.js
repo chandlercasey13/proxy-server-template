5f89798b299f926b14b5acf2bd6c0a4b9ebb572d
Chandler Casey <chandlercasey13@gmail.com>
2025-01-03 14:21:30 -0600

    Add dotenv and config, change moduls

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const express = require('express');
+import express from 'express'
+import dotenv from 'dotenv'
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+dotenv.config();
 
 app.use(express.json());
 
